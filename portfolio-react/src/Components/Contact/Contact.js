cb9b7cd5c5370102a0d5a017f8fd90c0649ea272
MeesumAshary <meesumashary@gmail.com>
2022-08-09 09:26:07 -0700

    Complete css for projects page

--- portfolio-react/src/Components/Contact/Contact.js
+++ portfolio-react/src/Components/Contact/Contact.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 function Contact(props) {
-    return (
-        <div>
-            This is contact page....
-        </div>
-    );
+	return (
+		<div>
+			<form>
+				<label class='your-name'>
+					Your Name:
+					<input id='your-name' type='name' name='email' type='text' />
+				</label>
+				<label class='your-email'>
+					Your email:
+					<input id='your-email' type='email' name='email' type='text' />
+				</label>
+				<label class='your-message'>
+					Your message:
+					<textarea id='textarea-message' name='message' type='text'></textarea>
+				</label>
+
+				<button type='submit'>Send</button>
+			</form>
+		</div>
+	);
 }
 
 export default Contact;
--- portfolio-react/src/Components/Projects/Projects.js
+++ portfolio-react/src/Components/Projects/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Projects.css';
+import { Link } from 'react-router-dom';
 
 function Projects(props) {
 	return (
@@ -8,11 +9,13 @@ function Projects(props) {
 				<div className='card-header'>
 					<h2>Moving Co, React-Java Blog App</h2>
 					<div className='card-img-wrapper'>
-						<img
-							className='card-img'
-							src={require('../../assets/Moving-co.png')}
-							alt='movingco app screenshot'
-						/>
+						<a href='https://movingco.netlify.app/'>
+							<img
+								className='card-img'
+								src={require('../../assets/Moving-co.png')}
+								alt='movingco app screenshot'
+							/>
+						</a>
 					</div>
 				</div>
 				<div className='card-content'>
@@ -22,17 +25,17 @@ function Projects(props) {
 					</h3>
 					<ul className='card-role-description'>
 						<li className='card-rd-list'>
-							Utilized Java to create a Backend Rest API with full CRUD
+							- Utilized Java to create a Backend Rest API with full CRUD
 							functionalities and JWT authentication and Authorization.
 						</li>
 						<li className='card-rd-list'>
-							Designed control React components that signed up and logged in
+							- Designed control React components that signed up and logged in
 							users. These users were configured to be authorized and create
 							blog posts.
 						</li>
 						<li className='card-rd-list'>
-							Utilized CSS to make the app responsive and utilized semantic HTML
-							to make App more accessible.
+							- Utilized CSS to make the app responsive and utilized semantic
+							HTML to make App more accessible.
 						</li>
 					</ul>
 				</div>
@@ -42,11 +45,13 @@ function Projects(props) {
 				<div className='card-header'>
 					<h2>Gitz Carlton- MERN Full CRUD App</h2>
 					<div className='card-img-wrapper'>
-						<img
-							className='card-img'
-							src={require('../../assets/Gitz-Carlton.png')}
-							alt='movingco app screenshot'
-						/>
+						<a href='https://cozy-gnome-816bc0.netlify.app/'>
+							<img
+								className='card-img'
+								src={require('../../assets/Gitz-Carlton.png')}
+								alt='movingco app screenshot'
+							/>
+						</a>
 					</div>
 				</div>
 				<div className='card-content'>
@@ -56,18 +61,18 @@ function Projects(props) {
 					</h3>
 					<ul className='card-role-description'>
 						<li className='card-rd-list'>
-							Designed controlled react components that creates, returns,
+							-Designed controlled react components that creates, returns,
 							updates and deletes guest data and allows user to assign a room to
 							the guest.
 						</li>
 						<li className='card-rd-list'>
-							Utilized git, GitHub to collaborate with fellow contributors by
+							-Utilized git, GitHub to collaborate with fellow contributors by
 							using a mix of Feature Branch workflow and GitFlow workflow
 							methods.
 						</li>
 						<li className='card-rd-list'>
-							Utilized CSS to make the app responsive and utilized semantic HTML
-							to make App more accessible.
+							-Utilized CSS to make the app responsive and utilized semantic
+							HTML to make App more accessible.
 						</li>
 					</ul>
 				</div>
@@ -77,30 +82,32 @@ function Projects(props) {
 				<div className='card-header'>
 					<h2>Moving Co, React-Java Blog Site</h2>
 					<div className='card-img-wrapper'>
-						<img
-							className='card-img'
-							src={require('../../assets/Moving-co.png')}
-							alt='movingco app screenshot'
-						/>
+						<a href='https://beamish-cassata-95fe05.netlify.app/'>
+							<img
+								className='card-img'
+								src={require('../../assets/MeesX.png')}
+								alt='movingco app screenshot'
+							/>
+						</a>
 					</div>
 				</div>
 				<div className='card-content'>
 					<h3>
-						<span className='skills-span'>Skills:</span> HTML, CSS, JavaScript
-						React, Axios , Postgresql, Java, SpringBoot{' '}
+						<span className='skills-span'>Skills:</span> HTML, CSS, JavaScript,
+						React, Axios
 					</h3>
 					<ul className='card-role-description'>
 						<li className='card-rd-list'>
-							Designed Single-Page React application that utilizes Axios to
-							fetch and display SpaceX API Data
+							-Designed Single-Page React application that utilizes Axios to
+							fetch and display SpaceX API Data{' '}
 						</li>
 						<li className='card-rd-list'>
-							Included 8 components and 2 forms of interaction by user: data
+							-Included 8 components and 2 forms of interaction by user: data
 							displayed and styled by CSS.
 						</li>
 						<li className='card-rd-list'>
-							Utilized CSS to make the app responsive and utilized semantic HTML
-							to make App more accessible.
+							-Utilized CSS to make the app responsive and utilized semantic
+							HTML to make App more accessible.
 						</li>
 					</ul>
 				</div>
